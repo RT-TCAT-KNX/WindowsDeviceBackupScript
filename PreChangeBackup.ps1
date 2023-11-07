7840ab90903f95090ed62bea98fe3822c82e21ed
Robert Tallent <robert.tallent@tcatknoxville.edu>
2023-11-07 17:57:28 -0500

    added starting point for filesize reader

--- PreChangeBackup.ps1
+++ PreChangeBackup.ps1
@@ -10,7 +10,8 @@ $winget_filename = -join("D:\",$folder_name,"\",$folder_name,"_Installed_Package
 winget list | Out-File $winget_filename
 #$user_data_filename = $host + "_UserData.txt"
 #$program_data_filename = $host + "_UserData.txt"
-
+$directory = Get-Item .
+$directory | Get-ChildItem | Measure-Object -Sum Length | Select-Object
 #check IF filesizes are too big
 #program data? -- FOR loop?
 #user  data? -- FOR loop?
