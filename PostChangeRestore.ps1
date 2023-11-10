eb980d9772f1733606384161921d003f39199fa5
Robert Tallent <robert.tallent@tcatknoxville.edu>
2023-11-10 08:46:09 -0500

    added AppData backup functionality

--- PostChangeRestore.ps1
+++ PostChangeRestore.ps1
@@ -1,17 +1,14 @@
-﻿#Comments like this
-
-Write-Host "Starting script..."
+﻿Write-Host "Starting script..."
 
 #variables
-$host = Convert-String -InputObject hostname #it's trying to re-write the hostname
-#throw error if drive not connected
 cd D:\
 $foldername = $env:COMPUTERNAME
-New-Item -Name $foldername -ItemType directory
-$winget_filename = $host + "_Installed_Packages.txt"
-$user_data_filename = $host + "_UserData.txt"
-$program_data_filename = $host + "_UserData.txt"
-winget list | Out-File $winget_filename
+$winget_filename = $env:COMPUTERNAME + "_Installed_Packages.txt"
+
+#for file in $winget_filename winget install $file
+
+#$user_data_filename = $host + "_UserData.txt"
+#$program_data_filename = $host + "_UserData.txt"
 #check IF filesizes are too big
 #program data? -- FOR loop?
 #user  data? -- FOR loop?
--- PreChangeBackup.ps1
+++ PreChangeBackup.ps1
@@ -4,15 +4,16 @@ Write-Host "Starting script..."
 
 #variables
 #TODO throw error if drive not connected
-$folder_name = $env:COMPUTERNAME
-New-Item -Path D:\ -Name $folder_name -ItemType directory
-$winget_filename = -join("D:\",$folder_name,"\",$folder_name,"_Installed_Packages.txt")
+$computer_name = $env:COMPUTERNAME
+$users_name = $env:UserName
+$app_data_filepath = -join("C:\Users\",$users_name,"\AppData")
+$backup_folder_name = -join("D:\",$computer_name,"\")
+$winget_filename = -join($backup_folder_name,$computer_name,"_Installed_Packages.txt")
+
+New-Item -Path D:\ -Name $computer_name -ItemType directory
 winget list | Out-File $winget_filename
-#$user_data_filename = $host + "_UserData.txt"
-#$program_data_filename = $host + "_UserData.txt"
-$directory = Get-Item .
-$directory | Get-ChildItem | Measure-Object -Sum Length | Select-Object
-#check IF filesizes are too big
-#program data? -- FOR loop?
-#user  data? -- FOR loop?
+Copy-Item $app_data_filepath -destination $backup_folder_name
+Write-Host "The script has run successfully!"
+
+#TODO check IF filesizes are too big
 #ELSE, throw error list other apps
