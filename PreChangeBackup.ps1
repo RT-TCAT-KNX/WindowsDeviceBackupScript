71a3af2c2216e048aed6330faec74944f3d561a9
Robert Tallent <robert.tallent@tcatknoxville.edu>
2024-02-07 16:20:53 -0500

    Prechange works

--- PreChangeBackup.ps1
+++ PreChangeBackup.ps1
@@ -1,27 +1,18 @@
-﻿function Confirm-Drive-Path {
-Do {
-$drive_response = Read-Host "Enter removable storage drive filepath (i.e. D:\, F:\, etc.)"
-$confirmation = Read-Host "You entered $drive_response. Is this correct? y/n: "
-}
-Until ($confirmation -eq "y" -and (Test-Path $drive_response))
-$global:my_drive = $drive_response
-}
-
-#variables
+﻿#variables
 
 $computer_name = $env:COMPUTERNAME
 $users_name = $env:UserName
 $appdata_filepath = -join("C:\Users\",$users_name,"\AppData")
-$backup_folder_name = -join($my_drive,$computer_name)
+$backup_folder_name = -join("D:\",$computer_name)
 $appdata_backup_folder_name = -join($backup_folder_name, "\AppData")
 $username_filename = -join($backup_folder_name,"\","username.txt")
-$winget_filename = -join($backup_folder_name,"\",$computer_name,"_Installed_Packages.txt")
+$winget_package_file = -join($backup_folder_name,"\",$computer_name,"_Installed_Packages.json")
+$winget_errors_log_file = -join($backup_folder_name,"\",$computer_name,"_Winget_Import_Errors_Log.txt")
 
 Write-Host "Starting script..."
-Confirm-Drive-Path
-New-Item -Path $my_drive -Name $computer_name -ItemType directory
+New-Item -Path "D:\" -Name $computer_name -ItemType directory
 $users_name | Out-File $username_filename
-winget list | Out-File $winget_filename
+winget export -o $winget_package_file --include-versions --accept-source-agreements | Out-File $winget_errors_log_file
 
 #Pick one of the lines below, but both are very slow. File Explorer is the fastest right now.
 #robocopy $appdata_filepath $backup_folder_name * /E /w:0 /r:1 /COPYALL /mt:8
